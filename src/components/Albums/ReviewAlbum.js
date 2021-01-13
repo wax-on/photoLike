4b0cf44eed047061a565804fabbc59a0ab66faf8
Hugo Johansson <hugo.johansson@hyperisland.se>
2021-01-13 15:21:25 +0100

    fixed so client can look at the images in review

--- src/components/Albums/ReviewAlbum.js
+++ src/components/Albums/ReviewAlbum.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import AlbumsImages from "./AlbumsImages";
+import useImages from "../../hooks/useImages";
+import useAlbum from "../../hooks/useAlbum";
 
 const ReviewAlbum = () => {
-  return <div>this is review album</div>;
+  const { albumId } = useParams();
+  const { album, loading } = useAlbum(albumId);
+  const { images } = useImages(albumId);
+
+  if (loading) {
+    return <p className="text-center">Loading...</p>;
+  }
+
+  return (
+    <>
+      <h5 className="mt-5 mb-5">
+        This is Album <strong>"{album.title}" </strong>
+        that you can Review
+      </h5>
+
+      <AlbumsImages images={images} />
+    </>
+  );
 };
 
 export default ReviewAlbum;
